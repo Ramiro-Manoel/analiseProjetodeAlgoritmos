41d414bf93589303dde3eb1c4a4270cc47be0534
Ramiro Manoel <ramiro.manoel@sap.com>
2025-08-18 20:51:11 -0300

    Selection sort implementation

---
+++ cmd/class03/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"analiseProjetodeAlgoritmos/internal/random"
+	"analiseProjetodeAlgoritmos/internal/sort"
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	var size int
+
+	fmt.Print("Array size: ")
+	if _, err := fmt.Scan(&size); err != nil {
+		log.Fatal("The array size must be a number")
+	}
+
+	array, err := random.RandomArrayGenerator(size)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(sort.SelectionSort(array))
+
+}
